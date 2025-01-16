ebae7817454934609b42005ea0c43781e60fd0e3
Tyler Mueller <TylerJacobMueller08@gmail.com>
2025-01-15 19:30:05 -0700

    Added command factory methods to elevatorsubsystem.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -3,8 +3,10 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.NeckConstants;
+import frc.robot.Constants.ElevatorConstants.ElevatorLevel;
 import frc.robot.Constants.ClimberConstants;
 import frc.robot.Constants.ElevatorConstants;
 
@@ -18,8 +20,6 @@ public class ElevatorSubsystem extends SubsystemBase {
                                                                 ElevatorConstants.ELEVATOR_PID.kD);
 
     private static final double[] setpoints = {0, 1000, 2000, 3000};
-
-    private double elevatorSetpoint = setpoints[0];
 
     private double getElevatorLevelSetpoint(ElevatorConstants.ElevatorLevel level) {
         switch (level){
@@ -38,6 +38,22 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     public void raiseElevatorToLevel(ElevatorConstants.ElevatorLevel level) {
         elevatorPID.setSetpoint(getElevatorLevelSetpoint(level));
+    }
+
+    public Command raiseElevatorToL1Command() {
+        return run(() -> raiseElevatorToLevel(ElevatorLevel.L1));
+    }
+
+    public Command raiseElevatorToL2Command() {
+        return run(() -> raiseElevatorToLevel(ElevatorLevel.L2));
+    }
+
+    public Command raiseElevatorToL3Command() {
+        return run(() -> raiseElevatorToLevel(ElevatorLevel.L3));
+    }
+
+    public Command raiseElevatorToL4Command() {
+        return run(() -> raiseElevatorToLevel(ElevatorLevel.L4));
     }
 
     @Override
